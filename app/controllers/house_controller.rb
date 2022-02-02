46f0a282595c4c4004e0cbf1a0f515893b7db3d1
Taras <pentsko.t@gmail.com>
2022-02-02 04:49:35 +0200

    add showing every house separately and theirs number of days

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -4,4 +4,13 @@ class HouseController < ApplicationController
         @house = House.all
     end
 
+    def show
+        @house = House.find_by id: params[:id]
+    end
+
+    def profit
+        @house = House.find_by id: params[:id]
+        @house.maximum(:price)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'house/:id', to: 'house#show'
   root to: 'house#index'
 end
